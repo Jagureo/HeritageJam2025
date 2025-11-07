8413602c694c53d8a9edb27dc15f778e5d106e9e
Jagureo <jagnzet@gmail.com>
2025-11-08 03:50:53 +0900

    Added timer display and stop timer if game stopped by user

--- HGJ/Scripts/UI/gameUI.gd
+++ HGJ/Scripts/UI/gameUI.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 @onready var station_label = $MainScreen/Sign/Station_Label
 @onready var happiness_label = $MainScreen/Sign2/Happniess_Level
 @onready var nextStationButton = $MainScreen/NextStationButton
+@onready var timerDisplay = $MainScreen/Sign3/Time_Left
 
 @onready var gameover_panel = $MainScreen/GameOverPanel
 @onready var gameover_happiness = $MainScreen/GameOverPanel/Happniess_Level
@@ -35,6 +36,7 @@ func showGameOverPanel(show : bool):
 		
 		nextStationButton.hide()
 		gameover_panel.show()
+		GameManager.sInstance.mStationWaitingTimer.stop()
 		
 	else:
 		gameover_panel.hide()
@@ -46,3 +48,6 @@ func OnPlayAgainButtonPressed() -> void:
 func OnMainMenuButtonPressed() -> void:
 	AudioManager.sInstance.mClickSound.play()
 	get_tree().change_scene_to_file(mMainMenuScene)
+	
+func _process(delta: float) -> void:
+	timerDisplay.text = "Time Remaining: %.1f" % GameManager.sInstance.mStationWaitingTimer.time_left
