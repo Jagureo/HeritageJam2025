a076a17aa849d752da719418944ee8bcfbebc8a2
Jia Zheng <yewjiazheng@gmail.com>
2026-06-08 00:58:09 +0800

    main menu screen transition animations

--- MRTRushHour/Scripts/UI/mainmenuUI.gd
+++ MRTRushHour/Scripts/UI/mainmenuUI.gd
@@ -4,7 +4,13 @@ extends Control
 @onready var mMainScreen : Control = $MainScreen
 @onready var mHowToPlayScreen : Control = $HowToPlayScreen
 @onready var mCreditsScreen : Control = $CreditsScreen
+@onready var mScreenAnimation : AnimationPlayer = $ScreenAnimations
 @export_file("*.tscn") var mGameScene : String
+
+
+
+func _ready():
+	mScreenAnimation.play("RESET")
 
 
 func OnPlayButtonPressed():
@@ -15,14 +21,12 @@ func OnPlayButtonPressed():
 
 func OnHowToPlayButtonPressed():
 	AudioManager.sInstance.mClickSound.play()
-	mMainScreen.visible = false
-	mHowToPlayScreen.visible = true
+	mScreenAnimation.play("FadeInH2P")
 
 	
 func OnCreditsButtonPressed():
 	AudioManager.sInstance.mClickSound.play()
-	mMainScreen.visible = false
-	mCreditsScreen.visible = true
+	mScreenAnimation.play("FadeInCredits")
 
 
 func OnBackButtonPressed():
@@ -30,3 +34,15 @@ func OnBackButtonPressed():
 	mMainScreen.visible = true
 	mHowToPlayScreen.visible = false
 	mCreditsScreen.visible = false
+
+
+func OnBackButtonPressed2(_lastScreen : String):
+	AudioManager.sInstance.mClickSound.play()
+	if _lastScreen == "credits":
+		mScreenAnimation.play("FadeOutCredits")
+	elif _lastScreen == "howtoplay":
+		mScreenAnimation.play("FadeOutH2P")
+	elif _lastScreen == "levelSelect":
+		mScreenAnimation.play("FadeOutCredits")
+	else:
+		assert(false, "Unknown screen animation being played")
